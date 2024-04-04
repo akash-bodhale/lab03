10b71e65b9c60178d0f629618d8dd0d6dc1f9132
akash bodhale <73112309+akash-bodhale@users.noreply.github.com>
2024-04-03 19:48:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
 function calc1(){
-    var numericRegex = /^[0-9]+$/
-    let total=parseFloat(document.getElementById("total").value)
+    var numericRegex = /^[0-9]+$/;
+    var restrictedRegex = /^[-+*/a-zA-Z\s]*$/;
+    let total=parseFloat(document.getElementById("total").value).toFixed(2)
     if(total>0)
     {
         console.log(total)
@@ -24,17 +24,35 @@ function calc1(){
             document.getElementById("tip").disabled = true
 
             document.getElementById("tipAmount").value = 0
-            document.getElementById("totalWithTip").value = 0
+            document.getElementById("totalWithTip").value = 0  
 
                  
         }
     }
-    else
+    else if(document.getElementById("total").value ==="")
     {
-        document.getElementById("errorMsg").innerHTML="you should put valid amount." 
+        document.getElementById("errorMsg").innerHTML="" 
+        document.getElementById("tip").disabled = true
+
+        document.getElementById("tipAmount").value = 0
+        document.getElementById("totalWithTip").value = 0   
+    }
+    else if(restrictedRegex.test(document.getElementById("total").value))
+    {
+        document.getElementById("errorMsg").innerHTML="please provide valid amount." 
+        document.getElementById("tip").disabled = true
+
+        document.getElementById("tipAmount").value = 0
+        document.getElementById("totalWithTip").value = 0
+
+    }
+    else if(total< 0)
+    {
+        document.getElementById("errorMsg").innerHTML="you Entered negative Number." 
         document.getElementById("tip").disabled = true
 
         document.getElementById("tipAmount").value = 0
         document.getElementById("totalWithTip").value = 0
     }
+    
 }
